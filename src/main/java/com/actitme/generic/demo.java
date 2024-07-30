d21c00e76ab5591ed6471494fc3738799f4825a1
santhosh0595 <santhosh0595@gmail.com>
2024-07-30 14:39:01 +0530

    demo class remove

--- src/main/java/com/actitme/generic/demo.java
+++
@@ -1,8 +0,0 @@
-package com.actitme.generic;
-
-public class demo {
-			public static void main(String[] args) {
-				System.out.println("update in genernic package");
-				System.out.println("hi hello");
-			}
-}
