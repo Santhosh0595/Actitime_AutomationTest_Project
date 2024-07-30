a9c80360f8621dc712bbacff183db47123766076
santhosh0595 <santhosh0595@gmail.com>
2024-07-30 14:35:47 +0530

    demo class updated

--- src/main/java/com/actitme/generic/demo.java
+++ src/main/java/com/actitme/generic/demo.java
@@ -3,5 +3,6 @@ package com.actitme.generic;
 public class demo {
 			public static void main(String[] args) {
 				System.out.println("update in genernic package");
+				System.out.println("hi hello");
 			}
 }
